33a8e1ab928d12f7299515764df62fcc80d7b41c
Petros Kontos <p_kontos@hotmail.com>
2021-09-11 20:25:04 +0300

    deleted commented out attempts for reeading data file

--- app/src/test/groovy/inventory/ItemTest.groovy
+++ app/src/test/groovy/inventory/ItemTest.groovy
@@ -6,40 +6,6 @@ class ItemTest extends Specification {
 	
 	def "file contents match constructor arguments"() {
 		
-<<<<<<< HEAD
-		given:
-		def name = "guitar"
-		def serialNo = "skfh58eHGt"
-		def value = 49.99
-		def item = new Item(name, serialNo, value)
-		
-		def file = new File("inventory.txt")
-		def dataFile = new DataFile(file.text)
-		
-		def detectionFlag = false;
-		
-//		expect:
-//		
-//		file.each {
-//			if (it == name + "," + serialNo + "," + value) {
-//				println int
-//				return true
-//			}
-//		}
-		
-//		when:
-//		file.each {
-//			line -> if ("$line" == name + "," + serialNo + "," + value) {		
-//				detectionFlag = true
-//			}
-//		}
-//		
-//		then:
-//		detectionFlag == true;
-		
-		expect:
-		dataFile.text == name + "," + serialNo + "," + value + '\n'
-=======
 		setup:
 		def name = "guitar"
 		def serialNo = "skfh58eHGt"
@@ -50,6 +16,5 @@ class ItemTest extends Specification {
 		item.name == name
 		item.serialNo == serialNo
 		item.value == value
->>>>>>> tmp
 	}
 }
