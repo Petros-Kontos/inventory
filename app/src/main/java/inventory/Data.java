e97ef22fafd153593e21c6dacab95697a04f0c6f
Petros Kontos <p_kontos@hotmail.com>
2021-09-11 11:33:22 +0300

    Enabled data file creation, with relevant test

--- app/src/main/java/inventory/Data.java
+++ app/src/main/java/inventory/Data.java
@@ -1,5 +1,17 @@
 package inventory;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Data {
 
+	public Data() {
+		File file = new File("src/test/resources/data.txt");
+		try {
+			file.createNewFile();
+		}
+		catch (IOException e) {  
+		e.printStackTrace();
+		}         
+	}
 }
--- app/src/test/groovy/inventory/DataSpec.groovy
+++ app/src/test/groovy/inventory/DataSpec.groovy
@@ -3,4 +3,14 @@ package inventory
 import spock.lang.Specification
 
 class DataSpec extends Specification {
+	
+	def "check if data file is created"() {
+		
+		setup:
+		def data = new Data()
+		File file = new File("src/test/resources/data.txt")
+		
+		expect:
+		file.exists() == true
+	}
 }
