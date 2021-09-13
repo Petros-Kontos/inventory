3eeb45802d033a92d9a7fcf022894346efb0de6a
Petros Kontos <p_kontos@hotmail.com>
2021-09-13 10:04:03 +0300

    Added assertion comments

--- app/src/test/groovy/inventory/ItemTest.groovy
+++ app/src/test/groovy/inventory/ItemTest.groovy
@@ -6,13 +6,13 @@ class ItemTest extends Specification {
 	
 	def "file contents match constructor arguments"() {
 		
-		setup:
+		setup: "store some testing values and instantiate an item object with them"
 		def name = "guitar"
 		def serialNo = "skfh58eHGt"
 		def value = 499.99
 		def item = new Item(name, serialNo, value)
 		
-		expect:
+		expect: "check the constructor has initialized the properties correctly"
 		item.name == name
 		item.serialNo == serialNo
 		item.value == value
