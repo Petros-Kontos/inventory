fe5f0bb20da4e493bb8ce6ab4cfcb589183c1f6c
Petros Kontos <p_kontos@hotmail.com>
2021-09-13 15:49:12 +0300

    added (failing) test for appending a single line/item to the data file

--- app/src/test/groovy/inventory/DataSpec.groovy
+++ app/src/test/groovy/inventory/DataSpec.groovy
@@ -13,6 +13,19 @@ class DataSpec extends Specification {
 		file.exists() == true
 	}
 	
+	def "append 1 item"() {
+		setup:
+		def name = "Buddha statue"
+		def serialNo = "kdhfo8374HLKD"
+		def value = 69.99
+		
+		when:
+		data.append(name, serialNo, value)
+		
+		then:
+		data.read() == name + "," + serialNo + "," + value + '\n'
+	}
+	
 	def "check data file deletion"() {
 		setup: "delete the data file"
 		file.delete()
