6b1cde4ad0bc214d48a7c0663f0b0991cc2c9fd8
Petros Kontos <p_kontos@hotmail.com>
2021-09-13 15:01:57 +0300

    added no-arg constructor test

--- app/src/test/groovy/inventory/OrchestratorSpec.groovy
+++ app/src/test/groovy/inventory/OrchestratorSpec.groovy
@@ -3,4 +3,13 @@ package inventory
 import spock.lang.Specification
 
 class OrchestratorSpec extends Specification {
+	
+	def "orchestrator class exists"() {
+		
+		setup: "instantiate an orchestr ator object"
+		def orchestrator = new Orchestrator()
+		
+		expect: "check orchestrator object has been created"
+		orchestrator != null
+	}
 }
